4bf9879d1fec72991fb9f2b7ebd178217d6ae4e3
guillenjoshua <guillenjoshua@gmail.com>
2020-11-29 12:07:02 -0600

    Fixed Project/Portfolio Cards

--- src/components/Cards/PortfolioCards.js
+++ src/components/Cards/PortfolioCards.js
@@ -5,9 +5,9 @@ function Cards () {
 
  return (
     <div className="background">
-
-            <div class="col-md-5 col-lg-5  firstCard">
-                    <div class="card">
+        <div className="row">
+            <div class="col-md-6 col-lg-6 ">
+                    <div class="card cardportfolio">
                         <img class="card-img-top" src="/img/IMG_1455.JPG" alt="Houston Skyline" />
                         <div class="card-body">
                             <h4 class="card-title">Resume</h4>
@@ -16,14 +16,15 @@ function Cards () {
                     </div>
             </div>
 
-            <div class="col-md-5 col-lg-5 secondCard">
-                    <div class="card">
+            <div class="col-md-6 col-lg-6 ">
+                    <div class="card cardportfolio">
                         <img class="card-img-top" src="/img/myisland.jpg" alt="Milos Island" />
                         <div class="card-body">
                             <h4 class="card-title">Projects</h4>
                             <a href="/projects" class="btn btn-outline-secondary">See Projects</a>
                     </div>
             </div>
+        </div>
     </div>
 
 
